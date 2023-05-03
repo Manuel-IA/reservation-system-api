997a70221eb94fd465c927ec8b49e9d3467b15c2
Manuel Esteban Palacio Calderón <manuelpalacio@wss.com.co>
2023-05-03 17:36:19 -0500

    Created scaffold for Reservations with command 'rails g scaffold Reservation datetime:datetime restaurant:references'

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,6 @@ class Restaurant < ApplicationRecord
 
   scope :filter_by_city, -> ( city ) { where('lower(city) LIKE :city', city: "#{city.strip.downcase}%") }
   scope :filter_by_name, -> ( name ) { where('lower(name) LIKE :name', name: "#{name.strip.downcase}%") }
+
+  has_many :reservations
 end
